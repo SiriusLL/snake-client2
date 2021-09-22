104736dc83a34bf7c780ad74df194421b7691450
SiriusLL <sirius.love.light@gmail.com>
2021-09-21 21:03:21 -0600

    refactor: move connect function to client.js file and import to play.js

--- play.js
+++ play.js
@@ -1,19 +1,3 @@
-const net = require("net");
+const { connect } = require("./client.js");
 
-// extablishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "192.168.0.125",
-    port: "50541",
-  });
-
-  conn.setEncoding("utf8");
-
-  conn.on("data", (data) => {
-    console.log(data);
-  });
-  return conn;
-};
-
-console.log("Connecting ...");
 connect();
